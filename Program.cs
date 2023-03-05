6bcde5508f5898037e79ad36b801522d15799c28
MSRbuddy <meghanadasari222@gmail.com>
2023-03-05 17:41:02 +0530

    Welcome to Employee-Payroll Services problem using ADO.NET!

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADO_Emp_Payroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Services using ADO!");
+        }
+    }
+}
